5831275d1b21efdafcc818fac2c166d0db559ba2
AV77web <villari.andra@libero.it>
2026-01-14 10:23:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//=====================================================
+// File: index.js
+// Script che gestisce la logica del backend
+//@author: andrea.villari@allievi.itsdigitalacademy.com
+//@version "1.0.0 2026-01-14"
+//=====================================================
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+};
+)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
