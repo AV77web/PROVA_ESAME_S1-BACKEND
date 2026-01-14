4c995f7ea7657ae628d0787fa6a542e3ffcac486
AV77web <villari.andrea@libero.it>
2026-01-14 14:37:45 +0100

    aggiornamento config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const FRONTEND_URL = isProduction
-    ? "https://frontend-phi-three-71.vercel.app"
+    ? "https://prova-esame-s1-frontend.vercel.app/"
     : "http://localhost:5173";
 
 module.exports = {
