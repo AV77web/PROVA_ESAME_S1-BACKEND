ffeefa1945e206b234f1b50e0a408ac9347a8928
AV77web <villari.andrea@libero.it>
2026-01-21 09:32:59 +0100

    ripristino del frontend con prova-esame-s1-frontend.vercel.app

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const FRONTEND_URL = isProduction
-    ? "https://andrea-villari.vercel.app"
+    ? "https://prova-esame-s1-frontenda.vercel.app"
     : "http://localhost:5173";
 
 module.exports = {
